3a018d0fe54f6f7471fe588bb8c4b15d24de0608
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-08-12 21:25:36 +0200

    small practice

--- app.js
+++ app.js
@@ -117,21 +117,6 @@ var canAlsoBeNull;
 canAlsoBeNull = null;
 var canThisBeAny = null; //now we can change type ?? in new version of ts
 canThisBeAny = 12;
-//js
-// let bankAccount = {
-//     money: 2000,
-//     deposit(value) {
-//         this.money += value;
-//     }
-// };
-// let myself = {
-//     name: "Max",
-//     bankAccount: bankAccount,
-//     hobbies: ["Sports", "Cooking"]
-// };
-// myself.bankAccount.deposit(3000);
-// console.log(myself);
-//ts
 var bankAccount = {
     money: 2000,
     deposit: function (value) {
